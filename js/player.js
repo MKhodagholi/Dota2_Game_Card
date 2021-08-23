a326f0b89106da37d840d06d4a0ef2596302a23d
Mkhodagholi <MKhodaagholi@gmail.com>
2021-08-23 17:22:48 +0430

    Change [function Player]

---
+++ js/player.js
@@ -0,0 +1,11 @@
+let player;
+
+function Player(health, mana, armor, str, agil, intel) {
+    this.health = health;
+    this.mana = mana;
+    this.mana = mana;
+    this.armor = armor;
+    this.str = str;
+    this.agil = agil;
+    this.intel = intel;
+}
